54d52ae6db22beb3a9350eb96abec87a2d10862e
mkbarnum <matthewkbarnum@gmail.com>
2020-10-10 17:33:43 -0600

    fixed first tests

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,5 @@
+import java.nio.charset.Charset;
+import java.util.Random;
 
 class Calculator {
 
@@ -39,7 +41,9 @@ class Calculator {
     etc
      */
     int fibonacciNumberFinder(int n){
-        return 0;
+        if (n <= 1)
+            return n;
+        return fibonacciNumberFinder(n-1) + fibonacciNumberFinder(n-2);
     }
 
 
@@ -51,7 +55,7 @@ class Calculator {
     if int a = 16 then this method returns: 10000
      */
     String intToBinaryNumber(int n){
-        return null;
+        return Integer.toBinaryString(n);
     }
 
     /*
@@ -63,8 +67,10 @@ class Calculator {
     if you run this function twice with the same String input, it must return 2 unique String IDs
      */
     String createUniqueID(String n){
-
-        return null;
+        byte[] array = new byte[7]; // length is bounded by 7
+        new Random().nextBytes(array);
+        String generatedString = new String(array, Charset.forName("UTF-8"));
+        return n + generatedString;
     }
 
 
